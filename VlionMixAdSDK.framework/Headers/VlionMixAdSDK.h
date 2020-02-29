76c3437db9d5d6b2bf6eb0d7b0c2787e8d9c1042
zhouxiaoxu <61242553@qq.com>
2020-02-29 17:41:54 +0800

    不依赖穿山甲、不依赖广点通

---
+++ VlionMixAdSDK.framework/Headers/VlionMixAdSDK.h
@@ -0,0 +1,19 @@
+//
+//  VlionMixAdSDK.h
+//  VlionMixAdSDK
+//
+//  Created by 周晓旭 on 2020/2/29.
+//  Copyright © 2020 周晓旭. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VlionMixAdSDK.
+FOUNDATION_EXPORT double VlionMixAdSDKVersionNumber;
+
+//! Project version string for VlionMixAdSDK.
+FOUNDATION_EXPORT const unsigned char VlionMixAdSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VlionMixAdSDK/PublicHeader.h>
+
+
